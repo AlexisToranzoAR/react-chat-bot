e85d477b566fcc190debb1e02f98fb6d0a46940c
Alexis Toranzo <alexistoranzoar@gmail.com>
2023-04-17 12:59:09 -0300

    Delete dist folder

--- dist/components/ChatBot/utils/getConversationIndex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = getConversationIndex;
-function getConversationIndex(conversation, id) {
-  return conversation.findIndex(i => i.id === id);
-}
